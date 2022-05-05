142c273692989503c07e6712cd8b8742d2d943bd
Jéssica Lopes Bruno <jessica.lopes.bruno@avanade.com>
2022-05-04 23:37:52 -0300

    Alterações nas tasks

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -9,32 +9,9 @@ import { addTask, getAllTasks } from "./utils/domFunctions.js";
 
 //
 
-const db = [
-    {
-        id: 1,
-        title: 'Concluir App Conexão Arte',
-        steps: [
-            {step: 'Ajustar textos'},
-            {step: 'Trocar imagerns para imagens públicas'},
-            {step: 'Publicar no Expo'},
-            {step: 'Publicar no Expo Store Connect'},
-        ],
-        done: false,
-        dueDate: '2022-05-06',
-        reminder: '2022-05-02 10:00', 
-    },
+const db = [];
 
-    {
-        id: 2,
-        title: 'Aula 4 Fiap - Avanade',
-        steps: [{step: 'Atributos Globais'},{step: 'Estrutura CSS'}],
-        done: false,
-        dueDate: '2022-05-06',
-        reminder: '2022-05-02 10:00', 
-    },
-];
-
-getAllTasks(db);
+// getAllTasks(db);
 
 // console.log(db[0].title);
 
